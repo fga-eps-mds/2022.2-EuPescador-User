00aefb324580e150fe340e95cae9f32252446f92
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-04-21 19:22:09 -0300

    create function on controller to verify token
    Co-authored-by: Felipe Moura <felipecandidodemoura@gmail.com>
    Co-authored-by: Luiza <luizaesteves987@gmail.com>

--- src/controllers/sendMailController.ts
+++ src/controllers/sendMailController.ts
@@ -3,6 +3,7 @@ import { sendMailService } from '../services/sendMailService';
 import { connection } from '../config/database';
 import { User } from '../models/user';
 import { Token } from '../models/token';
+import { FindOptionsWhere } from 'typeorm';
 
 export default class SendMailController {
   async sendMail(req: Request, res: Response) {
@@ -14,15 +15,43 @@ export default class SendMailController {
       const userRepository = connection.getRepository(User);
       const tokenRepository = connection.getRepository(Token);
 
-      const user = await userRepository.findOne({where: { email }});
+      const user = await userRepository.findOne({ where: { email } });
 
       const token = new Token();
       token.value = tokenValue;
-      token.user_id = String(user!.id); 
+      token.user_id = String(user!.id);
 
       await tokenRepository.save(token);
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async verifyToken(req: Request, res: Response) {
+    try {
+      const { value } = req.query as FindOptionsWhere<Token>;
+
+      const tokenRepository = connection.getRepository(Token);
+
+      const isTokenExists = await tokenRepository.findOne({
+        where: { value: value },
+      });
+
+      if (isTokenExists) {
+        await tokenRepository.remove(isTokenExists);
+        return res
+          .status(200)
+          .json({ message: 'Token encontrado!', token: isTokenExists });
+      } else {
+        return res.status(404).json({
+          message: 'Token não encontado!',
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({
+        message:
+          'Falha ao buscar token, tente gerar um token novamente e validá-lo!',
+      });
     }
   }
 }
